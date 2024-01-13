712499acd3279b01a93e463a2e907b6dae4bcb7b
Ahmed Farghal <me@asoli.dev>
2024-01-13 12:50:09 +0000

    zfunc

--- zsh/install.sh
+++ zsh/install.sh
@@ -13,8 +13,13 @@ for rcfile in "${ZDOTDIR:-$HOME}"/.dotfiles/zsh/runcoms/^README.md(.N); do
     ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 
+# Link completions
+ln -vfs ~/.dotfiles/zsh/zfunc ~/.zfunc
+
 # Set default shell
 chsh -s /bin/zsh
 
 # Set theme
 base16_rose-pine-moon
+
+echo "Don't forget to run compinit"
