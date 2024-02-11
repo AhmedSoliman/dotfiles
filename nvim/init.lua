6b2de2e97ff7e3a28cafb82d72a96279c36b622a
Ahmed Farghal <me@asoli.dev>
2024-02-11 11:34:54 +0000

    neovide and gitconfig

--- nvim/init.lua
+++ nvim/init.lua
@@ -182,6 +182,20 @@ vim.cmd([[
   endfunction
 ]])
 
+-- Neovide overrides
+if vim.g.neovide then 
+  -- Scroll animation
+  vim.g.neovide_scroll_animation_length = 0.05
+  vim.g.neovide_hide_mouse_when_typing = true
+  -- Keyboard bindings for clipboard actions
+  vim.keymap.set('n', '<D-s>', ':w<CR>') -- Save
+  vim.keymap.set('v', '<D-c>', '"+y') -- Copy
+  vim.keymap.set('n', '<D-v>', '"+P') -- Paste normal mode
+  vim.keymap.set('v', '<D-v>', '"+P') -- Paste visual mode
+  vim.keymap.set('c', '<D-v>', '<C-R>+') -- Paste command mode
+  vim.keymap.set('i', '<D-v>', '<ESC>l"+Pli') -- Paste insert mode
+end
+
 vim.cmd([[
   " Layered Config
   call SourceIfExists('~/.config/nvim/layers/private/vimrc.vim')
